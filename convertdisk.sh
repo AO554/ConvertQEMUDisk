614c6a5375242ce875679936035a0739230f1fad
knuxify <knuxify@gmail.com>
2020-05-04 12:45:01 +0200

    script: clean up

--- convertdisk.sh
+++ convertdisk.sh
@@ -1,72 +1,41 @@
-#!/bin/bash
-#'Arry woz here wait wot
+#!/usr/bin/env bash
 
-
-# The programs escape from my code nightmare
-choice_two()
-{
-   echo ""
-   echo "Goodbye!"
-   echo "Returning to shell."
-   echo ""
-   exit 0
-}
+# Check if qemu-img is installed
+if ! type qemu-img &>/dev/null; then echo "Error: qemu-img is not installed or not in PATH. Exiting."; exit 1; fi
 
 # Converts a Hyper-V Disk using qemu-img (assuming its installed)
-choice_one() 
-{
-echo ""
-echo "Hyper-V to QCOW2 Conversion"
-echo ""
-echo "This process requires that you have 'qemu-img' installed on your system"
-echo "if you do not have the required package, CTRL + C and Install it"
-echo ""
-echo -n "Enter the Path to the vhd/vhdx file: "
-read -r orig_path
-echo ""
-echo -n "Please enter a new path + the name and extension: "
-read -r new_path_file
-echo ""
-echo "Thanks! Conversion will start shortly."
-echo ""
-qemu-img convert -O qcow2 $orig_path $new_path_file
-echo "Conversion Complete! Please check that your file is there :]"
-echo "" 
-printf 'Press [ENTER] to go back to main menu...'
-read _
-main_menu
+converter() {
+	unset orig_path new_path_file
+	echo -e "\nHyper-V to QCOW2 Conversion\n"
+	until [[ -e "$orig_path" ]]; do
+		read -rep "Enter the Path to the vhd/vhdx file: " orig_path
+		[[ "$orig_path" == '~'* ]] && orig_path="$HOME${orig_path/\~/}"
+		[[ ! -e "$orig_path" ]] && echo "File does not exist."
+	done
+	read -rep "Please enter a new path + the name and extension: " new_path_file
+	[[ "$new_path_file" == '~'* ]] && new_path_file="$HOME${new_path_file/\~/}"
+	[[ ! -e "$(dirname $new_path_file)" ]] && mkdir -p "$(dirname $new_path_file)"
+	echo -e "Thanks! Conversion will start shortly.\n"
+	qemu-img convert -O qcow2 "$orig_path" "$new_path_file"
+	echo "Conversion Complete! Please check that your file is there :]"
+	echo 'Press any key to go back to main menu...'
+	read -rn1
 }
 
-main_menu() 
-{
-clear
-echo ""
-echo "Harri's Simplified Disk Converter"
-echo "For QEMU Related tasks"
-echo ""
+while true; do
+	unset main_option
+	clear
+	echo -e "\nHarri's Simplified Disk Converter\nFor QEMU Related tasks\n"
+	echo -e "\nWhat would you like to do?\n"
+	echo "(1) Convert Hyper-V to QCOW2"
+	echo -e "(2) Quit\n"
 
+	until [[ "$main_option" = "1" ]] || [[ "$main_option" = "2" ]]; do
+		read -rep "Choice: " main_option
+	done
 
-echo ""
-echo "What would you like to do?"
-echo ""
-echo "(1) Convert Hyper-V to QCOW2"
-echo "(2) Quit"
-echo ""
-
-
-echo -n "Choice: "
-read -r main_option
-}
-
-main_menu
-
-if [ $main_option = '1' ];
-then
-   choice_one
-else
-   if [ $main_option = '2' ];
-   then
-      choice_two
-   fi
-fi
-
+	case "$main_option" in
+		1) converter;;
+		2) echo -e "\nGoodbye!\nReturning to shell.\n"; exit 0;;
+	esac
+done
